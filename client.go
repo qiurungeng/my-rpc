67198b06872d7df6d90755bcfd419c96c054da0c
qiurungeng <708801794@qq.com>
2020-12-05 16:03:26 +0800

    add Client & Call

---
+++ client.go
@@ -0,0 +1,236 @@
+package myrpc
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"myrpc/codec"
+	"net"
+	"sync"
+)
+
+// 封装了结构体 Call 来承载一次 RPC 调用所需要的信息
+// 其中, Done 字段用于当结束调用时异步通知对方
+type Call struct {
+	Seq           uint64
+	ServiceMethod string		// format "<service>.<method>"
+	Args          interface{}	// arguments to function
+	Reply         interface{}	// reply from the function
+	Error         error			// if error occurs, it will be set
+	Done          chan *Call	// Strobes when call is templated
+}
+
+// 当调用结束时，会调用 call.done() 通知调用方
+func (call *Call) done() {
+	call.Done <- call
+}
+
+type Client struct {
+	codec        codec.Codec
+	option       *Option
+	sendingMutex sync.Mutex // 保证消息有序发送, 防止多请求报文混淆
+	header       codec.Header // 只在请求发送时需要, 可以服用, 每个客户端只需要一个
+	mutex        sync.Mutex // protect following
+	seq          uint64
+	pending      map[uint64]*Call // 存储未处理完的请求, 键是编号, 值是Call实例
+	closing      bool // user has called Close
+	shutdown     bool // server has told us to close
+}
+
+var ErrShutdown = errors.New("connection is shutdown")
+
+var _ io.Closer = (*Client)(nil)
+
+func (c *Client) Close() error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	if c.closing {
+		return ErrShutdown
+	}
+	c.closing = true
+	return c.codec.Close()
+}
+
+// Check if client is available
+func (c *Client) IsAvailable() bool {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	return !c.shutdown && !c.closing
+}
+
+func (c *Client) registerCall(call *Call) (uint64, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	call.Seq = c.seq
+	c.pending[call.Seq] = call
+	c.seq++
+	return call.Seq, nil
+}
+
+func (c *Client) removeCall(seq uint64) *Call {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	call := c.pending[seq]
+	delete(c.pending, seq)
+	return call
+}
+
+// terminateCalls: 服务端或客户端发生错误时调用
+// shutdown = true, 将错误属性通知所有 pending 状态的 call
+func (c *Client) terminateCalls(err error) {
+	c.sendingMutex.Lock()
+	defer c.sendingMutex.Unlock()
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.shutdown = true
+	for _, call := range c.pending {
+		call.Error = err
+		call.done()
+	}
+}
+
+//receive: 客户端接收消息
+func (c *Client) receive() {
+	var err error
+	for err == nil {
+		var h codec.Header
+		if err := c.codec.ReadHeader(&h); err != nil {
+			break
+		}
+		call := c.removeCall(h.Seq)
+		switch {
+		case call == nil:
+			err = c.codec.ReadBody(nil)
+		case h.Error != "":
+			call.Error = fmt.Errorf(h.Error)
+			err = c.codec.ReadBody(nil)
+			call.done()
+		default:
+			err = c.codec.ReadBody(call.Reply)
+			if err != nil {
+				call.Error = errors.New("reading body" + err.Error())
+			}
+			call.done()
+		}
+	}
+	c.terminateCalls(err)
+}
+
+func (c *Client) send(call *Call){
+	c.sendingMutex.Lock()
+	defer c.sendingMutex.Unlock()
+
+	// register
+	seq, err := c.registerCall(call)
+	if err != nil {
+		call.Error = err
+		call.done()
+		return
+	}
+
+	// prepare req header
+	c.header.ServiceMethod = call.ServiceMethod
+	c.header.Seq = seq
+	c.header.Error = ""
+
+	if err = c.codec.Write(&c.header, call.Args); err != nil {
+		call := c.removeCall(seq)
+		// call may be nil, it usually means that Write partially failed,
+		// client has received the response and handled
+		if call != nil {
+			call.Error = err
+			call.done()
+		}
+	}
+}
+
+
+// Dial: 客户端发送连接请求
+// 返回连接后的客户端实例 client
+func Dial(network, address string, opts ...*Option) (client *Client, err error) {
+	option, err := parseOption(opts...)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := net.Dial(network, address)
+	if err != nil {
+		log.Println("rpc client: fail to dial:", address, err)
+		return nil, err
+	}
+	defer func() {
+		if client == nil {
+			_ = conn.Close()
+		}
+	}()
+	return NewClient(conn, option)
+}
+
+
+func NewClient(conn net.Conn, opt *Option) (*Client, error) {
+	f := codec.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		err := fmt.Errorf("invalid codec type %s", opt.CodecType)
+		log.Println("rpc client: codec error:", err)
+		return nil, err
+	}
+	if err := json.NewEncoder(conn).Encode(opt); err != nil {
+		log.Println("rpc client: codec error:", err)
+		_ = conn.Close()
+		return nil, err
+	}
+	return newClientCodec(f(conn), opt), nil
+}
+
+func newClientCodec(c codec.Codec, opt *Option) *Client{
+	client := &Client{
+		seq: 1,
+		codec: c,
+		option: opt,
+		pending: make(map[uint64]*Call),
+	}
+	go client.receive()
+	return client
+}
+
+func parseOption(opts ...*Option) (*Option, error) {
+	if len(opts) == 0 || opts[0] == nil {
+		return DefaultOption, nil
+	}
+	if len(opts) == 1 {
+		return nil, errors.New("number of options is more than 1")
+	}
+	opt := opts[0]
+	opt.MagicNumber = DefaultOption.MagicNumber
+	if opt.CodecType == "" {
+		opt.CodecType = DefaultOption.CodecType
+	}
+	return opt, nil
+}
+
+
+// Go: invokes the function asynchronously
+// It returns the Call structure representing the invocation
+func (c *Client) Go(serviceMethod string, args, reply interface{}, done chan *Call) *Call {
+	if done == nil {
+		done = make(chan *Call, 10)
+	} else if cap(done) == 0 {
+		log.Panic("rpc client: done channel is unbuffered")
+	}
+	call := &Call{
+		ServiceMethod: serviceMethod,
+		Args: args,
+		Reply: reply,
+		Done: done,
+	}
+	c.send(call)
+	return call
+}
+
+// Call invokes the named function, waits for it to complete,
+// and returns its error status.
+func (c *Client) Call(serviceMethod string, args, reply interface{}) error {
+	call := <- c.Go(serviceMethod, args, reply, make(chan *Call, 1)).Done
+	return call.Error
+}
--- main/main.go
+++ main/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"myrpc"
-	"myrpc/codec"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -24,23 +23,40 @@ func main() {
 	addr := make(chan string)
 	go startServer(addr)
 
-	conn, _ := net.Dial("tcp", <-addr)
-	defer func() { _ = conn.Close() }()
+	client, _ := myrpc.Dial("tcp", <- addr)
+	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-	//send options
-	_ = json.NewEncoder(conn).Encode(myrpc.DefaultOption)
-	c := codec.NewGobCodec(conn)
+	//
+	////send options
+	//_ = json.NewEncoder(conn).Encode(myrpc.DefaultOption)
+	//c := codec.NewGobCodec(conn)
+	//
+	//for i := 0; i < 5; i++ {
+	//	h := &codec.Header{
+	//		ServiceMethod: "Foo.Sum",
+	//		Seq: uint64(i),
+	//	}
+	//	_ = c.Write(h, fmt.Sprintf("myrpc seq %d", h.Seq))
+	//	var reply string
+	//	_ = c.ReadHeader(h)
+	//	_ = c.ReadBody(&reply)
+	//	log.Println("reply:", reply)
+	//}
 
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "Foo.Sum",
-			Seq: uint64(i),
-		}
-		_ = c.Write(h, fmt.Sprintf("myrpc seq %d", h.Seq))
-		var reply string
-		_ = c.ReadHeader(h)
-		_ = c.ReadBody(&reply)
-		log.Println("reply:", reply)
+		wg.Add(1)
+		go func(i int ) {
+			defer wg.Done()
+			args := fmt.Sprintf("myrpc req %d", i)
+			var reply string
+
+			if err := client.Call("", args, &reply); err != nil {
+				log.Fatal(err)
+			}
+			log.Println("reply:", reply)
+		}(i)
 	}
+	wg.Wait()
 }
